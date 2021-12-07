4e6435d84065eac0f457ea43f74ca4d1341f7d7c
Markus Winkler <winkler.info@icloud.com>
2021-12-07 13:58:43 +0100

    support for deployment and continuous events

--- cs_rslog.py
+++ cs_rslog.py
@@ -12,12 +12,15 @@ import sys
 import socket
 import time
 import yaml
+import json
+# import pprint
+from operator import itemgetter, attrgetter
 from cefevent import CEFEvent
 from datetime import datetime, timedelta
 
 # Constants
 _LOGGER = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s %(levelname)s (%(threadName)s) [%(funcName)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -44,6 +47,299 @@ def syslog(message, level=LEVEL['notice'], facility=FACILITY['local3'],
     sock.sendto(data.encode(), (host, port))
     sock.close()
 
+
+def collect_rt(c1_url,
+               api_key,
+               start_time,
+               end_time):
+    """
+    Collect runtime events from Container Security within a given time range
+    
+    Parameters
+    ----------
+    c1_url
+    api_key
+    start_time
+    end_time
+
+    Raises
+    ------
+    Timeout
+        The request timed out.
+    HTTPError
+        An HTTP error occurred.
+    RequestException
+        There was an ambiguous exception that occurred while handling our request.
+    ValueError
+        Houston, we have a problem
+    """
+
+    # API query and response parsing here
+    cursor = ""
+    events = []
+    while True:
+        url = "https://container." + c1_url + "/api/events/sensors?" \
+            + "next=" + cursor \
+            + "&limit=" + str(25) \
+            + "&fromTime=" + start_time \
+            + "&toTime=" + end_time
+        post_header = {
+            "Content-Type": "application/json",
+            "Authorization": "ApiKey " + api_key,
+            "api-version": "v1",
+        }
+        try:
+            response = requests.get(
+                url, headers=post_header, verify=True
+            )
+
+            response.encoding = response.apparent_encoding
+            response.raise_for_status()
+        except requests.exceptions.Timeout as err:
+            _LOGGER.error(response.text)
+            raise SystemExit(err)
+        except requests.exceptions.HTTPError as err:
+            _LOGGER.error(response.text)
+            raise SystemExit(err)
+        except requests.exceptions.RequestException as err:
+            # catastrophic error. bail.
+            _LOGGER.error(response.text)
+            raise SystemExit(err)
+
+        response = response.json()
+        # Error handling
+        if "message" in response:
+            if response['message'] == "Invalid API Key":
+                _LOGGER.error("API error: {}".format(response['message']))
+                raise ValueError("Invalid API Key")
+
+        events_count = len(response.get('events', {}))
+        _LOGGER.debug("Number of events in result set: %d", events_count)
+        if events_count > 0:
+            for event in response.get('events', {}):
+                events.append(event)
+
+        cursor = response.get('next', "")
+        if cursor == "":
+            break
+
+    _LOGGER.info("{} runtime events received".format(str(len(events))))
+
+    return events
+
+
+def collect_dc(c1_url,
+               api_key,
+               start_time,
+               end_time):
+    """
+    Collect deployment and continuous events from Container Security within a given time range
+    
+    Parameters
+    ----------
+    c1_url
+    api_key
+    start_time
+    end_time
+
+    Raises
+    ------
+    Timeout
+        The request timed out.
+    HTTPError
+        An HTTP error occurred.
+    RequestException
+        There was an ambiguous exception that occurred while handling our request.
+    ValueError
+        Houston, we have a problem
+    """
+
+    # API query and response parsing here
+    cursor = ""
+    events = []
+    while True:
+        url = "https://container." + c1_url + "/api/events/evaluations?" \
+            + "next=" + cursor \
+            + "&limit=" + str(25) \
+            + "&fromTime=" + start_time \
+            + "&toTime=" + end_time
+        post_header = {
+            "Content-Type": "application/json",
+            "Authorization": "ApiKey " + api_key,
+            "api-version": "v1",
+        }
+        try:
+            response = requests.get(
+                url, headers=post_header, verify=True
+            )
+
+            response.encoding = response.apparent_encoding
+            response.raise_for_status()
+        except requests.exceptions.Timeout as err:
+            _LOGGER.error(response.text)
+            raise SystemExit(err)
+        except requests.exceptions.HTTPError as err:
+            _LOGGER.error(response.text)
+            raise SystemExit(err)
+        except requests.exceptions.RequestException as err:
+            # catastrophic error. bail.
+            _LOGGER.error(response.text)
+            raise SystemExit(err)
+
+        response = response.json()
+        # Error handling
+        if "message" in response:
+            if response['message'] == "Invalid API Key":
+                _LOGGER.error("API error: {}".format(response['message']))
+                raise ValueError("Invalid API Key")
+
+        events_count = len(response.get('events', {}))
+        _LOGGER.debug("Number of events in result set: %d", events_count)
+        if events_count > 0:
+            for event in response.get('events', {}):
+                events.append(event)
+
+        cursor = response.get('next', "")
+        if cursor == "":
+            break
+
+    _LOGGER.info("{} deployment and continuous events received".format(str(len(events))))
+
+    return events
+
+
+def cef_rt(event, facility, host, port):
+    """
+    Creates a CEF event from a runtime event
+    
+    Parameters
+    ----------
+    event
+    facility
+    host
+    port
+    """
+
+    # message format:
+    # CEF:Version|Device Vendor|Device Product|Rule ID|Name|Severity|Extension
+    # sample:
+    # CEF:0|Trend Micro|Cloud One Container Security|1.0|0|TM-00000006|(T1059.004)Terminal shell in container|5|Extension
+    # Extension: ruleID clusterID clusterName mitigation policyName k8s.ns.name k8s.pod.name proc.cmdline proc.pname container.id
+    #            container.image.tag container.image.repository container.image.digest
+
+    # pprint.pprint(event)
+
+    c = CEFEvent()
+
+    c.set_field('name', event['name'])
+    c.set_field('deviceVendor' , 'Trend Micro')
+    c.set_field('deviceProduct', 'Cloud One Container Security Runtime')
+    c.set_field('rt', event['timestamp'])
+    c.set_field('severity', str(LEVEL[event['severity']]))
+    c.set_field('message',
+        "details" + " " +
+        "ruleID=" + event['ruleID'] + " " +
+        "clusterID=" + event['clusterID'] + " " +
+        "clusterName=" + event['clusterName'] + " " +
+        "mitigation=" + event['mitigation'] + " " +
+        "policyName=" + event['policyName'] + " " +
+        "k8s.ns.name=" + event['k8s.ns.name'] + " " +
+        "k8s.pod.name=" + event['k8s.pod.name'] + " " +
+        "proc.cmdline=" + event['proc.cmdline'] + " " +
+        "proc.pname=" + event.get('proc.pname', '<NA>') + " " +
+        "container.id=" + event['container.id'] + " " +
+        "container.image.tag=" + event['container.image.tag'] + " " +
+        "container.image.repository=" + event['container.image.repository'] + " " +
+        "container.image.digest=" + event['container.image.digest'])
+
+    syslog(c, level=LEVEL[event['severity']], facility=FACILITY[facility], host=host, port=port)
+    _LOGGER.debug("Runtime event sent")
+
+
+def cef_dc(event, facility, host, port):
+    """
+    Creates CEF event(s) from deployment or continious events
+    
+    Parameters
+    ----------
+    event
+    facility
+    host
+    port
+    """
+
+    # message format:
+    # CEF:Version|Device Vendor|Device Product|Rule ID|Name|Severity|Extension
+    # sample:
+    # CEF:0|Trend Micro|Cloud One Container Security|1.0|0|TM-00000006|(T1059.004)Terminal shell in container|5|Extension
+    # Extension: action mitigation clusterID clusterName decision exceptions kind namespace operation
+    #            policyDefinitionName policyName reason.action reason.resources reason.rule reason.type
+
+    # pprint.pprint(event)
+
+    # If we don't have a reason, it's an allow and we populate the event with an empty reason
+    if len(event['reasons']) == 0:
+        reason = {'action': '<N/A>',
+                  'resources': [],
+                  'rule': '<N/A>',
+                  'type': 'Allow'}
+        cef_dc_reasons(event, reason, facility, host, port)
+    else:
+        for reason in event['reasons']:
+            cef_dc_reasons(event, reason, facility, host, port)
+
+
+def cef_dc_reasons(event, reason, facility, host, port):
+    """
+    Creates a CEF event from a deployment or continuous event and reason
+    
+    Parameters
+    ----------
+    event
+    reason
+    facility
+    host
+    port
+    """
+
+    # We don't have a severity in deploy or continuous events, so we set allow to `info`
+    # and everything else to `warning`
+    severity = ""
+    if (event['decision'] == 'allow'):
+        severity = LEVEL['info']
+    else:
+        severity = LEVEL['warning']
+
+    # Building one event per reason
+    c = CEFEvent()
+    c.set_field('name', reason['type'].capitalize())
+    c.set_field('deviceVendor' , 'Trend Micro')
+    c.set_field('deviceProduct', 'Cloud One Container Security Deploy')
+    c.set_field('rt', event['timestamp'])
+    c.set_field('severity', str(severity))
+    c.set_field('message',
+        "details" + " " +
+        "action=" + event.get('action', '<N/A>') + " " +
+        "mitigation=" + event.get('mitigation', '<N/A>') + " " +
+        "clusterID=" + event['clusterID'] + " " +
+        "clusterName=" + event['clusterName'] + " " +
+        "decision=" + event['decision'] + " " +
+        "exceptions=" + json.dumps(event.get('exceptions', [])) + " " +
+        "kind=" + event['kind'] + " " +
+        "namespace=" + event['namespace'] + " " +
+        "operation=" + event.get('operation', '<N/A>') + " " +
+        "policyDefinitionName=" + event['policyDefinitionName'] + " " +
+        "policyName=" + event.get('policyName', '<NA>') + " " +
+        "reason.action=" + reason['action'] + " " +
+        "reason.resources=" + json.dumps(reason['resources']) + " " +
+        "reason.rule=" + reason.get('rule', '<N/A>') + " " +
+        "reason.type=" + reason['type']
+    )
+    c.build_cef()
+    syslog(c, level=severity, facility=FACILITY[facility], host=host, port=port)
+    _LOGGER.debug("Deployment or continuous event sent")
+
+
 def collect(c1_url,
             api_key,
             host,
@@ -55,15 +351,12 @@ def collect(c1_url,
     
     Parameters
     ----------
-    none
-
-    Raises
-    ------
-    ValueError
-        Houston, we have a problem
-
-    Returns
-    -------
+    c1_url
+    api_key
+    host
+    port
+    facility
+    interval
     """
 
     _LOGGER.debug("Cloud One API endpoint: {}".format(c1_url))
@@ -74,108 +367,44 @@ def collect(c1_url,
         start_time = (datetime.utcnow() - timedelta(minutes=(interval + 1))).strftime("%Y-%m-%dT%H:%M:%SZ")
         end_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
-        # API query and response parsing here
-        cursor = ""
         events = []
-        while True:
-            url = "https://container." + c1_url + "/api/events/sensors?" \
-                + "next=" + cursor \
-                + "&limit=" + str(25) \
-                + "&fromTime=" + start_time \
-                + "&toTime=" + end_time
-            post_header = {
-                "Content-Type": "application/json",
-                "Authorization": "ApiKey " + api_key,
-                "api-version": "v1",
-            }
-            try:
-                response = requests.get(
-                    url, headers=post_header, verify=True
-                )
+        _LOGGER.info("Query deployment and continuous events")
+        events += collect_dc(c1_url,
+                             api_key,
+                             start_time,
+                             end_time)
 
-                response.encoding = response.apparent_encoding
-                response.raise_for_status()
-            except requests.exceptions.Timeout as err:
-                _LOGGER.error(response.text)
-                raise SystemExit(err)
-            except requests.exceptions.HTTPError as err:
-                _LOGGER.error(response.text)
-                raise SystemExit(err)
-            except requests.exceptions.RequestException as err:
-                # catastrophic error. bail.
-                _LOGGER.error(response.text)
-                raise SystemExit(err)
+        _LOGGER.info("Query runtime events")
+        events += collect_rt(c1_url,
+                             api_key,
+                             start_time,
+                             end_time)
 
-            response = response.json()
-            # Error handling
-            if "message" in response:
-                if response['message'] == "Invalid API Key":
-                    _LOGGER.error("API error: {}".format(response['message']))
-                    raise ValueError("Invalid API Key")
-
-            events_count = len(response.get('events', {}))
-            _LOGGER.debug("Number of events in result set: %d", events_count)
-            if events_count > 0:
-                for event in response.get('events', {}):
-                    events.append(event)
-
-            cursor = response.get('next', "")
-            if cursor == "":
-                break
-
-        _LOGGER.debug("{} Container Security runtime events received".format(str(len(events))))
-
-        # message format:
-        # CEF:Version|Device Vendor|Device Product|Rule ID|Name|Severity|Extension
-        # sample:
-        # CEF:0|Trend Micro|Cloud One Container Security|1.0|0|TM-00000006|(T1059.004)Terminal shell in container|5|Extension
-        # Extension: ruleID clusterID clusterName mitigation policyName k8s.ns.name k8s.pod.name proc.cmdline proc.pname container.id
-        #            container.image.tag container.image.repository container.image.digest
         if len(events) > 0:
-            # Since we might get an unorderd event list we need to check for the latest timestamp
-            # in the events list
+
             event_timestamp_max = last_timestamp_sent
-            for event in reversed(events):
+            for event in sorted(events, key=itemgetter('timestamp')):
                 event_timestamp = datetime.strptime(event['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
                 if (event_timestamp > last_timestamp_sent):
                     if (event_timestamp > event_timestamp_max):
                         event_timestamp_max = datetime.strptime(event['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
 
-                    c = CEFEvent()
-                    c.set_field('name', event['name'])
-                    c.set_field('deviceVendor' , "Trend Micro")
-                    c.set_field('deviceProduct', 'Cloud One Container Security')
-                    c.set_field('rt', event['timestamp'])
-                    c.set_field('severity', str(LEVEL[event['severity']]))
-                    c.set_field('message',
-                        "details" + " " +
-                        "ruleID=" + event['ruleID'] + " " +
-                        "clusterID=" + event['clusterID'] + " " +
-                        "clusterName=" + event['clusterName'] + " " +
-                        "mitigation=" + event['mitigation'] + " " +
-                        "policyName=" + event['policyName'] + " " +
-                        "k8s.ns.name=" + event['k8s.ns.name'] + " " +
-                        "k8s.pod.name=" + event['k8s.pod.name'] + " " +
-                        "proc.cmdline=" + event['proc.cmdline'] + " " +
-                        "proc.pname=" + event.get('proc.pname', '<NA>') + " " +
-                        "container.id=" + event['container.id'] + " " +
-                        "container.image.tag=" + event['container.image.tag'] + " " +
-                        "container.image.repository=" + event['container.image.repository'] + " " +
-                        "container.image.digest=" + event['container.image.digest']
-                    )
-                    c.build_cef()
-                    syslog(c, level=LEVEL[event['severity']], facility=FACILITY[facility], host=host, port=port)
+                    if (event.get('type', None) == "falco"):
+                        cef_rt(event, facility, host, port)
+                    else:
+                        cef_dc(event, facility, host, port)
 
-                    # Return results
-                    _LOGGER.debug("Event sent")
             last_timestamp_sent = event_timestamp_max
+
         time.sleep(interval * 60)
+    
 
 if __name__ == '__main__':
 
     with open("config.yml", "r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
+    _LOGGER.info("Event forwarder started.")
     collect(cfg['cloudone']['c1_url'],
             cfg['cloudone']['api_key'],
             cfg['server']['host'],
